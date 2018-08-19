c468b884c1b6904bb68ae36060d295860b6f8299
Bart Slinger <bartslinger@gmail.com>
2018-08-19 16:53:20 +0200

    some instructions to keep wifi config safe

--- wificonfig.h
+++ wificonfig.h
@@ -1,2 +1,13 @@
+/*
+
+After entering wifi config, make sure to run:
+git update-index --assume-unchanged wificonfig.h
+
+
+To start keeping track with git again, run:
+git update-index --no-assume-unchanged wificonfig.h
+
+*/
+
 const char* ssid = "********";
 const char* password = "********";
